1b91d1afa8ab06dec4d87f015ecb443a9cf8a4f8
Erick Rivera <yomellamoerick.771@gmail.com>
2024-10-24 21:05:50 -0300

    initial commit

---
+++ src/main/java/principal/App.java
@@ -0,0 +1,25 @@
+package principal;
+
+public class App {
+  public static void main(String[] args) {
+    /*
+     * 03 - EJERCICIO DE PROGRAMACIÓN JAVA Supermercado.
+     * Práctica con interfaces y polimorfismo Supermercado.
+     * Creación de Interfaces Suponga que debe crear
+     * distintas clases Java para describir los productos
+     * que vende un supermercado. Para unificar el código
+     * de los distintos programadores del equipo debe crear
+     * las siguientes Interfaces Java para describir algunas
+     * características de los productos. Interfaz EsLiquido
+     * Esta interfaz indica que los objetos creados a partir
+     * de la clase serán líquidos, y tendrá los siguientes
+     * métodos: public void setVolumen(double v);
+     * public double getVolumen(); public void setTipoEnvase(String env);
+     * public String getTipoEnvase(); Interfaz EsAlimento
+     * Esta interfaz indica que los objetos creados a partir de
+     * la clase serán alimentos, y tendrá los siguientes métodos:
+     * public void setCaducidad(LocalDate fc);
+     * public LocalDate getCaducidad(); public int getCalorias();
+     */
+  }
+}
