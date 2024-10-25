4d08b770a6b1bdda59fb92f1a5453a0f4dbd3058
Erick Rivera <yomellamoerick.771@gmail.com>
2024-10-24 21:25:52 -0300

    detergente creado

---
+++ src/main/java/modelo/Detergente.java
@@ -0,0 +1,80 @@
+package modelo;
+
+import interfaces.IconDescuento;
+import interfaces.IesLiquido;
+
+public class Detergente implements IesLiquido, IconDescuento {
+
+  public String marca;
+  public double precio;
+  private double volumen;
+  private String tipoEnvase;
+  private double descuento;
+
+  // El constructor solo recibe una marca y un precio
+  public Detergente(String marca, double precio){
+    this.marca = marca;
+    this.precio = precio;
+  }
+  
+  public String getMarca() {
+    return marca;
+  }
+
+
+  public void setMarca(String marca) {
+    this.marca = marca;
+  }
+
+
+  public double getPrecio() {
+    return precio;
+  }
+
+
+  public void setPrecio(double precio) {
+    this.precio = precio;
+  }
+
+  @Override
+  public void setDescuento(double desc) {
+    this.descuento = desc;
+  }
+
+  @Override
+  public double getDescuento() {
+    return descuento;
+  }
+
+  @Override
+  public double getPrecioDescuento() {
+    return precio - (precio * (descuento /100));
+  }
+
+  @Override
+  public void setVolume(double v) {
+    this.volumen = v;
+  }
+
+  @Override
+  public double getVolume() {
+    return volumen;
+  }
+
+  @Override
+  public void setTipoEnvase(String env) {
+    this.tipoEnvase = env;
+  }
+
+  @Override
+  public String getTipoEnvase() {
+    return tipoEnvase;
+  }
+
+  @Override
+  public String toString() {
+    return "Detergente [marca=" + marca + ", precio=" + precio + ", volumen=" + volumen + ", tipoEnvase=" + tipoEnvase + ", descuento=" + descuento + "]";
+  }
+
+
+}
